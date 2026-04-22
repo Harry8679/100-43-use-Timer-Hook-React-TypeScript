26a866eca60e69455f91f45449fe902e1c017ed6
Harry7986 <emarh.harry.code@gmail.com>
2026-04-22 13:25:40 +0200

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import TimerDemo from './components/TimerDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <TimerDemo />;
 }
 
-export default App
+export default App;
